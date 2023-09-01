c042af6de021d86a46666e84bc03f78f02e36177
Anis Momin <anis_momin@hotmail.com>
2023-09-01 14:26:59 +0500

    Agent: added google api and calculator as agent

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,56 @@
   //Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
   import { OpenAI } from "langchain/llms/openai";
 
-  //Import the PromptTemplate module
-  import { PromptTemplate } from "langchain/prompts";
+  //Import the agent executor module
+  import { initializeAgentExecutorWithOptions } from "langchain/agents";
 
-  //Import the Chains module
-  import { LLMChain } from "langchain/chains";
+  //Import the SerpAPI and Calculator tools
+  import { Calculator } from "langchain/tools/calculator";
+  import { SerpAPI } from "langchain/tools";
 
   //Load environment variables (populate process.env from .env file)
   import * as dotenv from "dotenv";
   dotenv.config();
 
   export const run = async () => {
+
       //Instantiante the OpenAI model 
       //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
-      const model = new OpenAI({ temperature: 0.9 });
+      const model = new OpenAI({ temperature: 0 });
 
-      //Create the template. The template is actually a "parameterized prompt". A "parameterized prompt" is a prompt in which the input parameter names are used and the parameter values are supplied from external input 
-      const template = "What is a good name for a company that makes {product}?";
+      //Create a list of the instatiated tools
+      const tools = [new SerpAPI(), new Calculator()];
 
-      //Instantiate "PromptTemplate" passing the prompt template string initialized above and a list of variable names the final prompt template will expect
-      const prompt = new PromptTemplate({ template, inputVariables: ["product"] });
+      //Construct an agent from an LLM and a list of tools
+      //"zero-shot-react-description" tells the agent to use the ReAct framework to determine which tool to use. The ReAct framework determines which tool to use based solely on the tool’s description. Any number of tools can be provided. This agent requires that a description is provided for each tool.
+      const executor = await initializeAgentExecutorWithOptions(
+      tools,
+      model);
+      console.log("Loaded agent.");
 
-      //Instantiate LLMChain, which consists of a PromptTemplate and an LLM. Pass the result from the PromptTemplate and the OpenAI LLM model
-      const chain = new LLMChain({ llm: model, prompt });
+      //Specify the prompt
+      // const input =
+      // "Who is Beyonce's husband?" +
+      // " What is his current age raised to the 0.23 power?";
 
-      //Run the chain. Pass the value for the variable name that was sent in the "inputVariables" list passed to "PromptTemplate" initialization call
-      const res = await chain.call({ product: "colorful socks" });
-      console.log({ res }); //{ res: { text: "\n\nSocks-A-Poppin'" } }
+
+      const input =
+      "Who is imran khan?" +
+      " What is his current age and how many years he spend in politics so far?";
+      console.log(`Executing with input "${input}"...`);
+
+      //Run the agent
+      const result = await executor.call({ input });
+
+      console.log(`Got output ${result.output}`);
+      //Response
+      // Loaded agent.
+      // Executing with input "Who is Beyonce's husband? What is his current age raised to the 0.23 power?"...
+      // Got output Jay-Z is 53 years old and his age raised to the 0.23 power is 2.4922032039503494.
+
+      // Loaded agent.
+      // Executing with input "Who is imran khan? What is his current age and how many years he spend in politics so far?"...
+      // Got output Imran Khan is 70 years old and has been in politics for over 25 years.
   };
 
   run();
