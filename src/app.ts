9b00f11d162b20dc7956244583d77b3342f15617
Anis Momin <anis_momin@hotmail.com>
2023-09-01 14:39:17 +0500

    Memory: stateless chains and agents are using memory to keep context of last conversation

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,66 @@
-  //Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
-  import { OpenAI } from "langchain/llms/openai";
+//Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
+import { OpenAI } from "langchain/llms/openai";
 
-  //Import the agent executor module
-  import { initializeAgentExecutorWithOptions } from "langchain/agents";
+//Import the BufferMemory module
+import { BufferMemory } from "langchain/memory";
 
-  //Import the SerpAPI and Calculator tools
-  import { Calculator } from "langchain/tools/calculator";
-  import { SerpAPI } from "langchain/tools";
+//Import the Chains module
+import { LLMChain } from "langchain/chains";
 
-  //Load environment variables (populate process.env from .env file)
-  import * as dotenv from "dotenv";
-  dotenv.config();
+//Import the PromptTemplate module
+import { PromptTemplate } from "langchain/prompts";
 
-  export const run = async () => {
+//Load environment variables (populate process.env from .env file)
+import * as dotenv from "dotenv";
+dotenv.config();
 
-      //Instantiante the OpenAI model 
-      //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
-      const model = new OpenAI({ temperature: 0 });
+export const run = async () => {
 
-      //Create a list of the instatiated tools
-      const tools = [new SerpAPI(), new Calculator()];
+    //Instantiate the BufferMemory passing the memory key for storing state 
+    const memory = new BufferMemory({ memoryKey: "chat_history" });
 
-      //Construct an agent from an LLM and a list of tools
-      //"zero-shot-react-description" tells the agent to use the ReAct framework to determine which tool to use. The ReAct framework determines which tool to use based solely on the tool’s description. Any number of tools can be provided. This agent requires that a description is provided for each tool.
-      const executor = await initializeAgentExecutorWithOptions(
-      tools,
-      model);
-      console.log("Loaded agent.");
+    //Instantiante the OpenAI model 
+    //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
+    const model = new OpenAI({ temperature: 0.9 });
 
-      //Specify the prompt
-      // const input =
-      // "Who is Beyonce's husband?" +
-      // " What is his current age raised to the 0.23 power?";
+    //Create the template. The template is actually a "parameterized prompt". A "parameterized prompt" is a prompt in which the input parameter names are used and the parameter values are supplied from external input 
+    //Note the input variables {chat_history} and {input}
+    const template = `The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+    Current conversation:
+    {chat_history}
+    Human: {input}
+    AI:`;
 
+    //Instantiate "PromptTemplate" passing the prompt template string initialized above
+    const prompt = PromptTemplate.fromTemplate(template);
 
-      const input =
-      "Who is imran khan?" +
-      " What is his current age and how many years he spend in politics so far?";
-      console.log(`Executing with input "${input}"...`);
+    //Instantiate LLMChain, which consists of a PromptTemplate, an LLM and memory. 
+    const chain = new LLMChain({ llm: model, prompt, memory });
 
-      //Run the agent
-      const result = await executor.call({ input });
+    //Run the chain passing a value for the {input} variable. The result will be stored in {chat_history}
+    const res1 = await chain.call({ input: "Hi! I'm Morpheus." });
+    console.log({ res1 });
 
-      console.log(`Got output ${result.output}`);
-      //Response
-      // Loaded agent.
-      // Executing with input "Who is Beyonce's husband? What is his current age raised to the 0.23 power?"...
-      // Got output Jay-Z is 53 years old and his age raised to the 0.23 power is 2.4922032039503494.
+    //Run the chain again passing a value for the {input} variable. This time, the response from the last run ie. the  value in {chat_history} will alo be passed as part of the prompt
+    const res2 = await chain.call({ input: "What's my name?" });
+    console.log({ res2 });
 
-      // Loaded agent.
-      // Executing with input "Who is imran khan? What is his current age and how many years he spend in politics so far?"...
-      // Got output Imran Khan is 70 years old and has been in politics for over 25 years.
-  };
+    //BONUS!!
+    const res3 = await chain.call({ input: "Which epic movie was I in and who was my protege?" });
+    console.log({ res3 });
 
-  run();
+    // Resposne
+    // {
+    //   res1: {
+    //     text: " Hi Morpheus, I'm AI. Nice to meet you! Do you have any questions for me?"
+    //   }
+    // }
+    // { res2: { text: ' Your name is Morpheus.' } }
+    // {
+    //   res3: {
+    //     text: ' I believe you were in The Matrix, and your protege was Neo.'
+    //   }
+    // }
+};
+
+run();
